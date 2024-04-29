58916522c0aad3ed516a61ab0e5f298cd5267ce6
adrien L <adrien.lafargue@saint-benigne.fr>
2024-04-29 16:04:32 +0200

    Push Project

--- src/func/file/disbox-file-manager.js
+++ src/func/file/disbox-file-manager.js
@@ -271,6 +271,19 @@ class DisboxFileManager {
         return this.getFile(parentPath);
     }
 
+    fetchDirectoryChildren(path) {
+        if (path === '') {
+            return this.fileTree.children || {};
+        } else {
+            const file = this.getFile(path);
+            if (!file || file.type !== 'directory') {
+                throw new Error(`Invalid path or not a directory: ${path}`);
+            }
+            return file.children || {};
+        }
+    }
+    
+
     async updateFile(path, changes) {
         const file = this.getFile(path, false);
         if (!file) {
